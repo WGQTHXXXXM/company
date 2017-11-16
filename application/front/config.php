c49ea4427359c452d0e1b0d9b4f6ad608f505d1e
ZinniaY <songwei@zhicheauto.com>
2017-11-16 17:20:40 +0800

    11-16

---
+++ application/front/config.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    // 模板
+    'template' 				 => [
+        'layout_on' => true,
+        'layout_name' => 'LayoutView'
+    ],
+
+    // 视图输出字符串内容替换
+    'view_replace_str'       => [
+        '__FRONT__SITE__' => 'http://http://www.gf.com/',
+        '__FRONT__STATIC__' => 'http://http://www.gf.com/static/'
+    ],
+
+];
